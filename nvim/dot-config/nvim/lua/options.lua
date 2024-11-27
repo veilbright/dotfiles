796215bbecca432fa5cafd94e84dcabaaeec42ed
veil <veilbright77@gmail.com>
2024-11-26 19:42:26 -0600

    Remove WSL clipboard from nvim and edit waybar clock widget

--- nvim/dot-config/nvim/lua/options.lua
+++ nvim/dot-config/nvim/lua/options.lua
@@ -7,7 +7,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
@@ -24,13 +24,8 @@ vim.opt.autoindent = true
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
+	vim.opt.clipboard = "unnamedplus"
 end)
-
--- Use clipboard with WSL
-vim.g.clipboard = {
-  cache_enabled = true,
-}
 
 -- Enable break indent
 vim.opt.breakindent = true
@@ -43,7 +38,7 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 -- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
 -- Decrease update time
 vim.opt.updatetime = 250
@@ -60,10 +55,10 @@ vim.opt.splitbelow = true
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '│ ', trail = '·', nbsp = '_' }
+vim.opt.listchars = { tab = "│ ", trail = "·", nbsp = "_" }
 
 -- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 
 -- Show which line your cursor is on
 vim.opt.cursorline = true
